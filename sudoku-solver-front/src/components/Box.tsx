9331766681fd48b97c62bdc339c4c5eecb43c80a
Andrea <andrea.zanibellato@gmail.com>
2022-10-01 21:24:18 +0200

    Added validation in frontend

--- sudoku-solver-front/src/components/Box.tsx
+++ sudoku-solver-front/src/components/Box.tsx
@@ -4,13 +4,24 @@ type BoxProps = {
     row: number;
     col: number;
     max: number;
+    error: boolean;
     onChange: (event: React.ChangeEvent<HTMLInputElement>, row: number, col: number) => void;
 };
 
 function Box(props: BoxProps){
+    const squareSize = Math.sqrt(props.max);
+    //Borders
+    const borderTop = (props.row % squareSize===0) ? ' border-top' : '';
+    const borderBottom = (props.row % squareSize===2)? ' border-bottom' : '';
+    const borderLeft = (props.col % squareSize===0)? ' border-left' : '';
+    const borderRight = (props.col % squareSize===2)? ' border-right' : '';
+    const error = (props.error) ? ' error' : '';
+    // CSS classes
+    const classes =`box${borderTop}${borderBottom}${borderLeft}${borderRight}${error}`;
+
     return <input 
         type="number"
-        className="box"
+        className={classes}
         min="1"
         max={props.max}
         value={props.value || ""}
--- sudoku-solver-front/src/components/Grid.tsx
+++ sudoku-solver-front/src/components/Grid.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import Box from './Box';
 import '../css/Grid.css';
 
@@ -8,19 +7,47 @@ type GridProps = {
     changeNumber: (event: React.ChangeEvent<HTMLInputElement>, row: number, col: number) => void;
 };
 
-function Grid(props: GridProps){
+function numberIsValid(row:number, col:number, data:number[][]):boolean {
+    // We don't check null values
+    if (data[row][col]==null) return true;
 
+    //Check row
+    for (let j=0; j<data.length; j++){ 
+        if (j!==col && data[row][j]===data[row][col]) 
+            return false;
+    }
+
+    //Check column
+    for (let i=0; i<data.length; i++){ 
+        if (i!==row && data[i][col]===data[row][col]) 
+        return false;
+    }
+
+    // Check subgrid (square)
+    const squareSize = Math.sqrt(data.length);
+    const rowSquareStart = Math.floor(row/squareSize) * squareSize;
+    const colSquareStart = Math.floor(col/squareSize) * squareSize;
+    for (let i=rowSquareStart; i<rowSquareStart+squareSize; i++){
+        for (let j=colSquareStart; j<colSquareStart+squareSize; j++){
+            if ((i!==row || j!==col) && data[i][j]===data[row][col]) 
+                return false;
+        }
+    }
+
+    //No errors, number is valid
+    return true;
+}
+
+function Grid(props: GridProps){
+    
     const rows = props.data.map(
         (row:number[], rowIndex:number)=> 
             (<div className="grid-row">
                 {row.map( (el:number, colIndex:number)=> 
                     (<Box onChange={props.changeNumber} row={rowIndex} col={colIndex} max={props.size}
-                        value={el} key={rowIndex*props.size+colIndex} />)) }
+                        value={el} key={rowIndex*props.size+colIndex} error={!numberIsValid(rowIndex, colIndex, props.data)}/>)) }
             </div>)
         );
-        
-    console.log(rows);
-
 
     return (
     <div className="grid">
