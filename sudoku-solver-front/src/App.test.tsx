0c13d9658ee8f962912c22cdf1bafabbcf353684
Andrea <andrea.zanibellato@gmail.com>
2022-10-07 11:01:05 +0200

    Fixed test but with Typescript

--- sudoku-solver-front/src/App.test.tsx
+++ sudoku-solver-front/src/App.test.tsx
@@ -7,7 +7,7 @@ describe('initial appeareance',()=>{
   test('solve button should be disabled on start', () => {
     render(<App />);
     const solveButton:HTMLButtonElement = screen.getByRole("button", { name: "Solve" });
-    expect(solveButton).toBeDisabled();
+    expect(solveButton.disabled).toBe(true);
   });
 
 })
